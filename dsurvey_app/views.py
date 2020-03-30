1ef953ba119cc08be1f07782903cc6767cdd7328
John Doe <johndoe@example.com>
2020-03-30 02:03:35 -0700

    Updated views.py and urls.py

--- dsurvey_app/views.py
+++ dsurvey_app/views.py
@@ -4,16 +4,20 @@ def index(request):
     return render(request, 'index.html')
 
 def process(request):
-    if request.method == "POST":
-        context = {
-            'name': request.POST['name'],
-            'lang': request.POST['language'],
-            'loca': request.POST['location']
-        }
-        return render(request, "result.html", context)
-    return render(request, "result.html")
+    if request.method != "POST":
+        return redirect('/')
+    request.session['name'] = request.POST['name']
+    request.session['language'] = request.POST['language']
+    request.session['location'] = request.POST['location']
+    return redirect(f'/results')
     
-
+def results(request):
+    # context = {
+    #     'name': request.session['name'],
+    #     'language': request.session['language'],
+    #     'location': request.session['location']
+    # }
+    return render(request, 'results.html')
 
 
 
